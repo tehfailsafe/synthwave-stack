10cd2e29a6e7048de3d2c266cd3b11600a93a69d
mikeatoctoml <80989449+mikeatoctoml@users.noreply.github.com>
2023-05-25 18:12:56 -0500

    add realtime subscription

---
+++ app/utils/useRealtime.tsx
@@ -0,0 +1,22 @@
+import { useRevalidator } from "@remix-run/react";
+import { useEffect } from "react";
+import { useSupabase } from "~/supabaseContext";
+
+export const useRealtime = (table: string) => {
+  const supabase = useSupabase();
+  const revalidator = useRevalidator();
+
+  return useEffect(() => {
+    supabase
+      .channel("any")
+      .on(
+        "postgres_changes",
+        { event: "*", schema: "public", table: table },
+        () => {
+          console.log("revalidating");
+          revalidator.revalidate();
+        }
+      )
+      .subscribe();
+  }, []);
+};
