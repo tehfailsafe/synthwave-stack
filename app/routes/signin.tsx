14647d0fc722617a2e510cd2ef349a5b4523b363
mikeatoctoml <80989449+mikeatoctoml@users.noreply.github.com>
2023-06-23 20:53:42 -0500

    use client side login function

--- app/routes/signin.tsx
+++ app/routes/signin.tsx
@@ -1,40 +1,20 @@
-import { useNavigate } from "@remix-run/react";
 import { useAuth } from "~/utils/AuthProvider";
 
 export default function Signin() {
-  const { signIn, signUp } = useAuth();
-  const navigate = useNavigate();
-
-  const handleSignin = async (e: Event) => {
-    e.preventDefault();
-
-    await signUp({
-      email: "test@test.com",
-      password: "password",
-    });
-
-    navigate("/");
-  };
+  const { signInWithGoogle } = useAuth();
 
   return (
     <div className="container mx-auto flex justify-center items-center flex-col h-full w-1/2">
       <h1 className="text-4xl mb-8">Sign In</h1>
-      <form onSubmit={(e) => handleSignin(e)} className="w-1/2 flex flex-col">
-        <input
-          className="block px-2 py-1 bg-transparent border border-gray-700 rounded-sm mb-4"
-          type="email"
-          name="email"
-          placeholder="Email"
-        />
 
-        <input
-          className="block px-2 py-1 bg-transparent border border-gray-700 rounded-sm mb-8"
-          type="password"
-          name="password"
-          placeholder="Password"
-        />
-        <button className="bg-blue-500 py-2 rounded-sm">Submit</button>
-      </form>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => {
+          signInWithGoogle();
+        }}
+      >
+        Sigin with Google
+      </button>
     </div>
   );
 }
